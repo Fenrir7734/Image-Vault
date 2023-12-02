680ea6020ae3d90db0833e6f3755a4aded149aab
Fenrir7734 <hitex1999@gmail.com>
2023-12-02 21:41:12 +0100

    IV-15 larger icons and reteining page number

--- frontend/src/app/shared/components/paginator/paginator.component.ts
+++ frontend/src/app/shared/components/paginator/paginator.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { Pagination } from '../../models/page/page';
 import { Direction, PageRequest } from '../../models/page/page-request';
 
@@ -7,9 +7,18 @@ import { Direction, PageRequest } from '../../models/page/page-request';
   templateUrl: './paginator.component.html',
   styleUrls: ['./paginator.component.scss'],
 })
-export class PaginatorComponent {
+export class PaginatorComponent implements OnInit {
   @Input() pagination: Pagination | null | undefined;
   @Output() pageChange = new EventEmitter<PageRequest>();
+  first: number;
+
+  ngOnInit() {
+    if (this.pagination) {
+      this.first = this.pagination.size * this.pagination.pageable.pageNumber;
+    } else {
+      this.first = 0;
+    }
+  }
 
   onPageChange(event: any) {
     if (!this.pagination) {
